a149c5fde8f65fc036996dc18a650aca9f13eb0e
silviofranco <silviofrancoms@gmail.com>
2025-01-05 16:42:40 -0400

    Criando metodos post put patch e delete

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:ffi';
 
 import 'package:consumo_servico_avancado/post.dart';
 import 'package:flutter/material.dart';
@@ -21,11 +20,54 @@ class _HomeState extends State<Home> {
 
     List<Post> postagens = List.empty(growable: true);
     for (var post in dadosJson) {
-      print("post: " + post["title"]);
       Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
       postagens.add(p);
     }
     return postagens;
+  }
+
+  _post() async {
+    Post post = Post(120, 0, "Title", "Corpo da postagem");
+
+    http.Response response = await http.post(
+      Uri.parse(_urlBase + "/posts"),
+      headers: {"Content-type": "application/json; charset=UTF-8"},
+      body: json.encode(post.toJson()),
+    );
+    print("Resposta Status Code: " + response.statusCode.toString());
+    print("Resposta: " + response.body);
+  }
+
+  _put() async {
+    http.Response response = await http.put(
+      Uri.parse("$_urlBase/posts/2"),
+      headers: {"Content-type": "application/json; charset=UTF-8"},
+      body: json.encode({
+        "userId": 120,
+        "id": null,
+        "title": "Teste alterado",
+        "body": "Teste alterado"
+      }),
+    );
+    print("Resposta Status Code: " + response.statusCode.toString());
+    print("Resposta: " + response.body);
+  }
+
+  _patch() async {
+    http.Response response = await http.patch(
+      Uri.parse("$_urlBase/posts/2"),
+      headers: {"Content-type": "application/json; charset=UTF-8"},
+      body: json.encode({"userId": 120, "body": "Teste alterado patch"}),
+    );
+    print("Resposta Status Code: " + response.statusCode.toString());
+    print("Resposta: " + response.body);
+  }
+
+  _delete() async {
+    http.Response response =
+    await http.delete(Uri.parse(_urlBase + "/posts/2"));
+    print("Resposta Status Code: " + response.statusCode.toString());
+    print("Resposta: " + response.body);
   }
 
   @override
@@ -35,41 +77,68 @@ class _HomeState extends State<Home> {
         title: Text("Consumo de serviço avançado"),
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _recuperarPostagens(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(child: CircularProgressIndicator());
-              break;
-            case ConnectionState.active:
-            case ConnectionState.done:
-              print("Conexão done");
-              if (snapshot.hasError) {
-                print("Lista: Erro ao carregar");
-              } else {
-                print("Lista carregada com sucesso");
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: _post,
+                  child: Text("Salvar"),
+                ),
+                ElevatedButton(
+                  onPressed: _put,
+                  child: Text("Att"),
+                ),
+                ElevatedButton(
+                  onPressed: _patch,
+                  child: Text("Att patch"),
+                ),
+                ElevatedButton(
+                  onPressed: _delete,
+                  child: Text("Rem"),
+                ),
+              ],
+            ),
+            Expanded(
+              child: FutureBuilder<List<Post>>(
+                future: _recuperarPostagens(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                      break;
+                    case ConnectionState.active:
+                    case ConnectionState.done:
+                      print("Conexão done");
+                      if (snapshot.hasError) {
+                        print("Lista: Erro ao carregar");
+                      } else {
+                        return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            List<Post> lista = snapshot.data!;
+                            Post post = lista[index];
 
-                    List<Post> lista = snapshot.data!;
-                    Post post = lista[index];
-
-                    return ListTile(
-                      title: Text(post.title),
-                      subtitle: Text(post.id.toString()),
-                    );
-                  },
-                );
-              }
-              break;
-          }
-          return Center(
-              //child: Text(_resultado),
-              );
-        },
+                            return ListTile(
+                              title: Text(post.title),
+                              subtitle: Text(post.id.toString()),
+                            );
+                          },
+                        );
+                      }
+                      break;
+                  }
+                  return Center(
+                    //child: Text(_resultado),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/post.dart
+++ lib/post.dart
@@ -1,11 +1,19 @@
-class Post{
-
+class Post {
   int _userId;
   int _id;
   String _title;
   String _body;
 
   Post(this._userId, this._id, this._title, this._body);
+
+  Map<String, dynamic> toJson() {
+    return {
+      "userId": this._userId,
+      "id": this._id,
+      "title": this._title,
+      "body": this._body
+    };
+  }
 
   String get body => _body;
 
